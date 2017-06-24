c5063b5c373bd36bc375d08798802a0a19e6db20
Rochester <jrw2175@columbia.edu>
2017-06-24 16:56:10 -0400

    Remove Terraform functionality
    - Not going to use Gitlab terraform template for deploying Gitlab
      anymore due to the new, not well-documented nature of the feature

--- opsimulate/cli.py
+++ opsimulate/cli.py
@@ -2,15 +2,11 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2017 James Wen
 
-from Crypto.PublicKey import RSA
 import json
 import os
 from subprocess import call
-import zipfile
 
 import click
-import git
-from requests import get
 
 
 @click.group()
@@ -21,12 +17,8 @@ def cli():
 @cli.command('deploy')
 def deploy():
     # Set max charge/budget constrictions on student’s GCP account
-    # Use Terraform and Gitlab terraform template to deploy and setup
-    _generate_ssh_key()
-    _add_ssh_key_for_terraform()
-    _setup_terraform()
-    _get_gitlab_terraform()
-    # _run_terraform()
+    # Use GCE API client and Gitlab debian package to deploy and setup
+    _add_service_account_ssh_key()
     # Setup the student’s investigative tools on the server: ensure common
     # operational tools like curl, iostat, lsop, lsof, w, dig, nslookup, mtr,
     # etc. are present
@@ -36,23 +28,7 @@ def deploy():
     pass
 
 
-def _generate_ssh_key():
-    DIR_NAME = "./keys"
-    if not os.path.isdir(DIR_NAME):
-        print("Generating SSH key")
-        os.mkdir(DIR_NAME)
-
-        key = RSA.generate(2048)
-        with open("./keys/private.key", 'w') as content_file:
-            os.chmod("./keys/private.key", 0600)
-            content_file.write(key.exportKey('PEM'))
-        pubkey = key.publickey()
-        with open("./keys/public.key", 'w') as content_file:
-            content_file.write(pubkey.exportKey('OpenSSH'))
-        print("Generated SSH key")
-
-
-def _add_ssh_key_for_terraform():
+def _add_service_account_ssh_key():
     service_account_file = 'service-account.json'
     if os.path.isfile(service_account_file):
         print("Adding SSH key to project now")
@@ -68,40 +44,3 @@ def _add_ssh_key_for_terraform():
         print("Your GCP project's owner service account's credentials must be "
               "in the '{}' file.".format(service_account_file))
         exit(1)
-
-
-def _setup_terraform():
-    """
-    Setup the dependencies necessary to deploy the simulation software
-    """
-    if not os.path.isfile('terraform'):
-        # Download terraform zip
-        print("Downloading Terraform now...")
-        terraform_download_link = 'https://releases.hashicorp.com/terraform/' \
-                                  '0.9.10/terraform_0.9.10_darwin_amd64.zip'
-        terraform_zip_file = 'terraform.zip'
-        with open(terraform_zip_file, "wb") as file:
-            response = get(terraform_download_link)
-            file.write(response.content)
-
-        # unzip
-        zip_ref = zipfile.ZipFile(terraform_zip_file, 'r')
-        zip_ref.extractall('.')
-        zip_ref.close()
-        # terraform binary is available
-        print("Terraform is ready")
-
-        os.remove(terraform_zip_file)
-
-
-def _get_gitlab_terraform():
-    DIR_NAME = "/tmp/gitlab-terraform"
-    if not os.path.isdir(DIR_NAME):
-        print("Fetching gitlab GCE terraform repo")
-        REMOTE_URL = "https://gitlab.com/gitlab-terraform/gce.git"
-        os.mkdir(DIR_NAME)
-        repo = git.Repo.init(DIR_NAME)
-        origin = repo.create_remote('origin', REMOTE_URL)
-        origin.fetch()
-        origin.pull(origin.refs[0].remote_head)
-        print("Fetched gitlab GCE terraform repo")
