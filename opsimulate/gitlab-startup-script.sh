b27ffba65d303efec03829fd4f0d633301b091a1
Rochester <jrw2175@columbia.edu>
2017-07-01 21:22:47 -0400

    Ensure common system and network debugging tools are available on VM
    - mpstat, iostat, traceroute, ntop, htop

--- opsimulate/gitlab-startup-script.sh
+++ opsimulate/gitlab-startup-script.sh
@@ -2,8 +2,13 @@
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y postfix
 
+# System and network debugging tools
+sudo apt-get install -y git htop sysstat traceroute
+
+# Gitlab dependencies
 sudo apt-get install -y curl openssh-server ca-certificates
 
+# Gitlab installation
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
 sudo apt-get install gitlab-ce
 
